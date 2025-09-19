51c3eae2d43ece4510c38fd7f4d31c1055496f79
JorgeCKR <jorgeckr@gmail.com>
2025-09-19 19:23:33 +0000

    Auto agrega el user

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,174 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [lista, setLista] = useState([])
+	const [tarea, setTarea] = useState("")
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	useEffect(() => {
+
+		console.log("Funcion del useEffect llamada");
+
+		obtenerTarea();
+		agregaUsuario();
+	}, [])
+
+	function obtenerTarea() {
+
+		fetch('https://playground.4geeks.com/todo/users/JorgeCKR')
+
+			.then(resp => {
+				console.log(resp);
+				return resp.json();
+			})
+			.then(data => {
+
+				console.log(data.todos);
+				setLista(data.todos)
+			})
+
+			.catch(error => {
+				console.log(error);
+			});
+
+	}
+
+	function agregaUsuario() {
+
+		fetch('https://playground.4geeks.com/todo/users/JorgeCKR', {
+			method: 'POST',
+			headers: {
+				'Content-Type': "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp);
+				return resp.json();
+			})
+			.then(data => {
+
+				console.log(data);
+				obtenerTarea();
+			})
+			.catch(error => {
+
+				console.log(error);
+			});
+	}
+
+	const agregaLista = (e) => {
+
+		e.preventDefault()
+
+		if (tarea.trim() === "") {
+			return;
+		}
+
+		fetch('https://playground.4geeks.com/todo/todos/JorgeCKR', {
+			method: "POST",
+			body: JSON.stringify({
+				label: tarea,
+				is_done: false
+			}),
+
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp);
+				console.log();
+			})
+			.then(data => {
+
+				console.log(data);
+				obtenerTarea();
+			})
+			.catch(error => {
+
+				console.log(error);
+			});
+
+		setTarea("");
+	}
+
+	const borraLista = (id) => {
+
+		fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+			method: "DELETE",
+			body: JSON.stringify({
+				is_done: true
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+
+				if (resp.status === 204) {
+					return null;
+				}
+
+				return resp.json();
+			})
+			.then(data => {
+
+				obtenerTarea();
+			})
+			.catch(error => {
+
+				console.log(error);
+			});
+	};
+
+	const actualizaTarea = (valorInput) => {
+
+		setTarea(valorInput.target.value)
+	}
+
+	return (
+		<form
+			onSubmit={agregaLista}
+			className=" ">
+			<div className="card mx-auto">
+				<div className="card-body">
+					<h1>
+						Lista To-Do
+					</h1>
+					<ul className="list-group list-group-flush">
+						<li className="list-group-item d-flex align-items-center justify-content-between">
+							<input
+								onChange={actualizaTarea}
+								value={tarea}
+								type="text"
+								className="PrimerValor input border-0 shadow-none"
+								placeholder="Agrega tu tarea..."
+							/>
+						</li>
+						{lista.length === 0 ? (
+							<li className="TareaHTML list-group-item"></li>
+						) : (
+							lista.map((item) => (
+								<li
+									key={item.id}
+									className="list-group-item d-flex justify-content-between align-items-center"
+								>
+									{item.label}
+									<button
+										type="button"
+										className="btn-close"
+										aria-label="Close"
+										onClick={() => borraLista(item.id)}
+									></button>
+								</li>
+							))
+						)}
+						<div className="card-footer text-body-secondary">
+							Tienes {lista.length} por hacer
+						</div>
+					</ul>
+				</div>
+			</div>
+		</form>
 	);
 };
 
